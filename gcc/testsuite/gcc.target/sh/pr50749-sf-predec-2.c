71c4f16b30f8d1c87e79cd349454e481a1dc940b
Oleg Endo <olegendo@gcc.gnu.org>
2016-04-24 18:50:23 +0900

    add and tune test cases.

--- gcc/testsuite/gcc.target/sh/pr50749-sf-predec-2.c
+++ gcc/testsuite/gcc.target/sh/pr50749-sf-predec-2.c
@@ -2,7 +2,7 @@
    are generated.  */
 /* { dg-do compile { target { any_fpu } } }  */
 /* { dg-options "-O2" } */
-/* { dg-final { scan-assembler-times "fmov.s\tfr\[0-9]\+,@-r\[0-9]\+" 5 } } */
+/* { dg-final { scan-assembler-times "fmov.s\tfr\[0-9]\+,@-r\[0-9]\+" 13 } } */
 
 float*
 test_func_00 (float* p, float c)
@@ -19,4 +19,13 @@ test_func_01 (float* p, float c)
   *--p = c;
   *--p = c;
   return p;
+}
+
+void
+foo (float mat[])
+{
+  float *p = &mat[16];
+
+  *--p = 0;  *--p = 0;  *--p = 0;  *--p = 0;
+  *--p = 0;  *--p = 0;  *--p = 0;  *--p = 0;
 }
